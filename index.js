83a856aa9579f8c5951ab2d1c8bc25e6d2ef00ad
Jitendra <jitendra01432@gmail.com>
2024-12-11 18:43:00 +0530

    route create

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 
-const express= require("express");
+
+const express=require("express")
 const app=express();
+const port=8000;
 
-
-
-
-
-app.listen(8080,()=>{
-
-    console.log("server run on 8080")
+app.get("/",(req,res)=>{
+         res.send("<h1> this is home page </h1>")
 })
+app.get("/home",(req,res)=>{
+        res.send("<h1> this is main home page </h1>")
+})
+app.listen(port,()=>{
+`this is server on${port}`})
+
+
